5e1b3c730f2a45216ba67980eebd1c389f1f35a5
shaguy3 <shaguy3@gmail.com>
2020-11-28 18:24:36 +0200

    Initial commit. Started working on the Date() class.

---
+++ date.cpp
@@ -0,0 +1,4 @@
+#include "date.h"
+
+Date::Date(int _day, int _month, int _year) :
+    day(_day), month(_month), year(_year) {}
---
+++ date.h
@@ -0,0 +1,15 @@
+#include <iostream>
+
+class Date {
+    private:
+        int day, month, year;
+    public:
+        Date(int _day, int _month, int _year);
+        int getDay();
+        int getMonth();
+        int getYear();
+        bool setDay(int _day);
+        bool setMonth(int _month);
+        bool setYear(int _year);
+        ~Date();
+};
