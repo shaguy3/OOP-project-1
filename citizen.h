1a84747d1de00da33846ec6ab5b905a401f0557b
WingLiveS <65483774+WingLiveS@users.noreply.github.com>
2020-12-05 18:18:59 +0200

    Update citizen.h

--- citizen.h
+++ citizen.h
@@ -1,22 +1,30 @@
 #pragma once
-#include "county.h"
+
+#include <iostream>
+//#include "county.h"
+#include <string.h>
+using namespace std;
+
 
 class Citizen {
-    private:
-        char* name;
-        int id;
-        int year_of_birth;
-        int home_county_number;
-    
-    public:
-        Citizen(char* _name, int _id, int year_of_birth, int home_county_number);
-        bool setName(char* _name);
-        char* getName();
-        bool setId(int _id);
-        int getId();
-        bool setYearOfBirth(int _year_of_birth);
-        int getYearOfBirth();
-        bool setHomeCounty(int _home_county_number);
-        int getHomeCounty();
-        ~Citizen();
+private:
+    char* name;     
+    int id;
+    int year_of_birth;
+    int home_county_number;
+
+public:
+    /*Constructors & Destructors*/
+    Citizen(char* _name, int _id, int _year_of_birth = 0, int _home_county_number = 0);
+    ~Citizen();
+    /*Getters*/
+    char* getName() const { return name; }
+    int getId() const { return id; }
+    int getYearOfBirth() const { return year_of_birth; }
+    int getHomeCounty() const { return home_county_number; }
+    /*Setters*/
+    bool setYearOfBirth(int _year_of_birth);        //return true if citizen is eligible to vote (At least 18 years old)
+    bool setCounty(int _home_county_number);        //return true if the county if valid
+    /*Operators*/
+    friend ostream& operator<<(ostream& os, const Citizen& other);
 };
