edb001a1a2a382f9a77b8c64576b90adc7892800
WingLiveS <65483774+WingLiveS@users.noreply.github.com>
2020-12-10 15:13:24 +0200

    Update county.h

--- county.h
+++ county.h
@@ -6,31 +6,33 @@ class Citizen;
 
 class County {
 
-    private:
-        char* name;
-        int id;
-        int number_of_electors;
-        Citizen** residents;
-        int residents_num_size;
-        int residents_num_logi;
-        Citizen** chosen_electors;
-        int chosen_electors_size;
-        int chosen_electors_logi;
-        void resizeResidents();
-        void resizeChosenElectors();
-    
-    public:
-        static int num_of_counties;
-        County(char* _name, int _number_of_electors);
-        char* getName();
-        int getId();
-        int getNumberOfElectors();
-        bool addResident(Citizen* new_resident);
-        Citizen** getResidents();
-        int residentsLen();
-        bool addChosenElector(Citizen* chosen_elector);
-        Citizen** getChosenElectors();
-        int chosenElectorsLen();
-        friend ostream& operator<<(ostream& os, const County& county);
-        ~County();
+private:
+    int current_vote_amount;
+    char* name;
+    int id;
+    int number_of_electors;
+    Citizen** residents;
+    int residents_num_size;
+    int residents_num_logi;
+    Citizen** chosen_electors;
+    int chosen_electors_size;
+    int chosen_electors_logi;
+    void resizeResidents();
+    void resizeChosenElectors();
+
+public:
+    static int num_of_counties;
+    County(char* _name, int _number_of_electors);
+    char* getName();
+    int getVoteAmount() { return current_vote_amount; }
+    int getId();
+    int getNumberOfElectors();
+    bool addResident(Citizen* new_resident);
+    Citizen** getResidents();
+    int residentsLen();
+    bool addChosenElector(Citizen* chosen_elector);
+    Citizen** getChosenElectors();
+    int chosenElectorsLen();
+    friend ostream& operator<<(ostream& os, const County& county);
+    ~County();
 };
