848304eac1e628e13bcd77b27b1f947ccf9dba71
WingLiveS <65483774+WingLiveS@users.noreply.github.com>
2020-12-10 15:11:34 +0200

    Update citizen.h

--- citizen.h
+++ citizen.h
@@ -6,29 +6,30 @@
 
 using namespace std;
 class County;
+class Party;
 
 class Citizen {
-    private:
-        char* name;     
-        int id;
-        int year_of_birth;
-        County* home_county;
-        bool is_representative;
-        bool has_voted;
+private:
+    char* name;
+    int id;
+    int year_of_birth;
+    County* home_county;
+    bool is_representative;
+    Party* has_voted;
 
-    public:
-        /*Constructors & Destructors*/
-        Citizen(char* _name, int _id, int _year_of_birth, County* _home_county);
-        ~Citizen();
-        /*Getters*/
-        char* getName() const { return name; }
-        int getId() const { return id; }
-        int getYearOfBirth() const { return year_of_birth; }
-        County* getHomeCounty() const { return home_county; }
-        bool isRepresentative() const { return is_representative; }
-        /*Setters*/
-        bool makeRepresentative();
-        bool setVoted();
-        /*Operators*/
-        friend ostream& operator<<(ostream& os, const Citizen& other);
+public:
+    /*Constructors & Destructors*/
+    Citizen(char* _name, int _id, int _year_of_birth, County* _home_county);
+    ~Citizen();
+    /*Getters*/
+    char* getName() const { return name; }
+    int getId() const { return id; }
+    int getYearOfBirth() const { return year_of_birth; }
+    County* getHomeCounty() const { return home_county; }
+    bool isRepresentative() const { return is_representative; }
+    /*Setters*/
+    bool makeRepresentative();
+    bool setVoted(Party* party_name);
+    /*Operators*/
+    friend ostream& operator<<(ostream& os, const Citizen& other);
 };
