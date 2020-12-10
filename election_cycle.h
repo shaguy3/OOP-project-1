dfe9eaafa69e443378deb0ae62f20023ae00d4d4
WingLiveS <65483774+WingLiveS@users.noreply.github.com>
2020-12-10 15:14:32 +0200

    Update election_cycle.h

--- election_cycle.h
+++ election_cycle.h
@@ -9,41 +9,43 @@
 using namespace std;
 
 class ElectionCycle {
-    private:
-        Date date_of_election;
-        Citizen** residents;
-        int residents_num_size;
-        int residents_num_logi;
-        Party** parties;
-        int parties_num_size;
-        int parties_num_logi;
-        County** counties;
-        int counties_num_size;
-        int counties_num_logi;
-        void resizeResidents();
-        void resizeParties();
-        void resizeCounties();
+private:
+    int current_vote_amount;        //Increases when voting 
+    Date date_of_election;
+    Citizen** residents;
+    int residents_num_size;
+    int residents_num_logi;
+    Party** parties;
+    int parties_num_size;
+    int parties_num_logi;
+    County** counties;
+    int counties_num_size;
+    int counties_num_logi;
+    void resizeResidents();
+    void resizeParties();
+    void resizeCounties();
 
-    public:
-        /* Constructors and destructors */
-        ElectionCycle(Date& _date_of_election);
-        ~ElectionCycle();
-        /* Getters and setters */
-        Date getDate();
-        Citizen** getResidents();
-        Party** getParties();
-        County** getCounties();
-        int residentslen();
-        int partieslen();
-        int countieslen();
-        /* Get specific items from the arrays */
-        Citizen* getResident(int id);
-        Party* getParty(char* party_name);
-        County* getCounty(int id);
-        /* Adders */
-        bool addResident(Citizen* resident);
-        bool addParty(Party* party);
-        bool addCounty(County* county);
-        /* Operators */
-        friend ostream& operator<<(ostream& os, const ElectionCycle& election_cycle);
+public:
+    /* Constructors and destructors */
+    ElectionCycle(Date& _date_of_election);
+    ~ElectionCycle();
+    /* Getters and setters */
+    Date getDate();
+    Citizen** getResidents();
+    Party** getParties();
+    County** getCounties();
+    int getVoteAmount() { return current_vote_amount; }
+    int residentslen();
+    int partieslen();
+    int countieslen();
+    /* Get specific items from the arrays */
+    Citizen* getResident(int id);
+    Party* getParty(char* party_name);
+    County* getCounty(int id);
+    /* Adders */
+    bool addResident(Citizen* resident);
+    bool addParty(Party* party);
+    bool addCounty(County* county);
+    /* Operators */
+    friend ostream& operator<<(ostream& os, const ElectionCycle& election_cycle);
 };
