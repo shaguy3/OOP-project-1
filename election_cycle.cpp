71c70354cae9d60d596a5a4decf09229acc416f6
WingLiveS <65483774+WingLiveS@users.noreply.github.com>
2020-12-11 23:32:30 +0200

    Update election_cycle.cpp

--- election_cycle.cpp
+++ election_cycle.cpp
@@ -1,17 +1,18 @@
 #include "election_cycle.h"
 
 ElectionCycle::ElectionCycle(Date& _date_of_election) :
-    date_of_election(_date_of_election) ,
+    date_of_election(_date_of_election),
+    current_vote_amount(0),
     residents_num_size(5),
     residents_num_logi(0),
     parties_num_size(5),
     parties_num_logi(0),
     counties_num_size(5),
     counties_num_logi(0),
-    residents(new Citizen*[residents_num_size]),
-    parties(new Party*[parties_num_size]),
-    counties(new County*[counties_num_size])
-    {}
+    residents(new Citizen* [residents_num_size]),
+    parties(new Party* [parties_num_size]),
+    counties(new County* [counties_num_size])
+{}
 
 ElectionCycle::~ElectionCycle() {
     delete[] residents;
@@ -21,39 +22,33 @@ ElectionCycle::~ElectionCycle() {
 
 void ElectionCycle::resizeResidents() {
     residents_num_size *= 2;
-    Citizen** new_arr = new Citizen*[residents_num_size];
+    Citizen** new_arr = new Citizen * [residents_num_size];
 
     for (int i = 0; i < residents_num_logi; i++) {
         new_arr[i] = residents[i];
     }
-
-    delete[] residents;
 
     residents = new_arr;
 }
 
 void ElectionCycle::resizeParties() {
     parties_num_size *= 2;
-    Party** new_arr = new Party*[parties_num_size];
+    Party** new_arr = new Party * [parties_num_size];
 
     for (int i = 0; i < parties_num_logi; i++) {
         new_arr[i] = parties[i];
     }
-
-    delete[] parties;
 
     parties = new_arr;
 }
 
 void ElectionCycle::resizeCounties() {
     counties_num_size *= 2;
-    County** new_arr = new County*[counties_num_size];
+    County** new_arr = new County * [counties_num_size];
 
     for (int i = 0; i < counties_num_logi; i++) {
         new_arr[i] = counties[i];
     }
-
-    delete[] counties;
 
     counties = new_arr;
 }
@@ -61,7 +56,7 @@ void ElectionCycle::resizeCounties() {
 Citizen* ElectionCycle::getResident(int id) {
     int cur = 0;
     do {
-        if(residents[cur]->getId() == id) { return residents[cur]; }
+        if (residents[cur]->getId() == id) { return residents[cur]; }
         else { cur++; }
     } while (cur < residents_num_logi);
 
@@ -117,8 +112,8 @@ bool ElectionCycle::addCounty(County* county) {
 
 ostream& operator<<(ostream& os, ElectionCycle& election_cycle) {
     os << "Election cycle of date: " << election_cycle.getDate() << endl \
-       << "Residents: " << endl;
-    
+        << "Residents: " << endl;
+
     for (int i = 0; i < election_cycle.residentslen(); i++) {
         os << *election_cycle.getResidents()[i] << endl;
     }
