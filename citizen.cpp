2abef0992f3452a819cee8a5caf1fdeddfadf444
WingLiveS <65483774+WingLiveS@users.noreply.github.com>
2020-12-15 15:12:11 +0200

    Update citizen.cpp

--- citizen.cpp
+++ citizen.cpp
@@ -4,7 +4,7 @@ Citizen::Citizen(char* _name, int _id, int _year_of_birth, County* _home_county)
     : name(new char[strlen(_name) + 1]),
     id(_id), year_of_birth(_year_of_birth),
     home_county(_home_county),
-    is_representative(false),
+    is_representative(nullptr),
     has_voted(nullptr)
 {
     int cur_char = 0;
@@ -24,12 +24,12 @@ std::ostream& operator<<(std::ostream& os, const Citizen& other)
 {
     os << "Name: " << other.name << endl << "Id: " << other.id << endl \
         << "Year of birth: " << other.year_of_birth << endl \
-        << "Home county: " << other.home_county->getName()  << "(ID: " << other.home_county->getId() << ")" << endl;
+        << "Home county: " << other.home_county->getName() << "(ID: " << other.home_county->getId() << ")" << endl;
     return os;
 }
 
-bool Citizen::makeRepresentative() {
-    is_representative = true;
+bool Citizen::makeRepresentative(Party* party_name) {
+    is_representative = party_name;
 
     return true;
 }
